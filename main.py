9e745b0f0765b66e7041931ee3cc9c4078e1a3be
shou-gakusei <2219579737@qq.com>
2025-03-25 12:28:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
+import zipfile
 
+def screenshot_compress(reader_id,quantities):
+    with zipfile.ZipFile(reader_id,"w") as zipF:
+        for screenshot_count in range(quantities):
+            zipF.write(reader_id,os.path.basename(str(reader_id+"_"+str(screenshot_count+1)+".png")))
 
 def save_images(url, folder,img_count):
     # 设置浏览器选项
@@ -12,7 +17,7 @@ def save_images(url, folder,img_count):
     options.headless = True  # 无头模式
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')  # 设置窗口大小
-    while not os.path.exists(str(folder) + "_" + str(img_count) + ".png"):
+    while not os.path.exists("C:/Users/shou-gakusei/PycharmProjects/hitomidownloader/images/"+str(folder)+"_"+str(img_count)+".png"):
         # 初始化浏览器驱动
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
@@ -37,7 +42,7 @@ def save_images(url, folder,img_count):
             time.sleep(1)
 
             # 保存全屏截图
-            img_path = str(folder)+"_"+str(img_count)+".png"
+            img_path = "C:/Users/shou-gakusei/PycharmProjects/hitomidownloader/images/"+str(folder)+"_"+str(img_count)+".png"
             driver.save_screenshot(img_path)
             print(f'截图已保存为：{os.path.abspath(img_path)}')
 
@@ -59,7 +64,12 @@ def save_images(url, folder,img_count):
 def save_reader_images(reader_id,quantities):
     hitomi_url = "https://hitomi.la/reader/"
     reader_url = ".html#"
-    for img_count in range(quantities-1):
+    for img_count in range(quantities):
         save_images(hitomi_url+str(reader_id)+reader_url+str(img_count+1),folder=str(reader_id),img_count=img_count+1)
 
-save_reader_images(3267993,451)
+image_series_list = ["3278803.html#960", "3267537.html#1445", "3269200.html#255", "3257320.html#865"]
+#测试用例
+#这部分等有空了再说
+
+
+# save_reader_images(3278803,960)
